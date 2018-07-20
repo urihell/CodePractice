33a08eff771c9305151f5ca971699f4a67153ca9
Uriel <urihell@gmail.com>
2018-07-20 16:13:42 -0400

    MyMainPackage and SubPackage packages

--- myprogram.py
+++ myprogram.py
@@ -1,3 +1,7 @@
-from mymodule import my_func
+from MyMainPackage import some_main_script
+from MyMainPackage.MySubPackage import mysubscript
 
-my_func()
+
+some_main_script.report_main()
+
+mysubscript.sub_report()
