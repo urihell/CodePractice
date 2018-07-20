3df3a28dca2e2938bdabd119e1bc8d4a62c24157
Uriel <urihell@gmail.com>
2018-07-19 23:10:58 -0400

    Add bank account class

---
+++ Bank_Account.py
@@ -0,0 +1,47 @@
+class Account:
+
+    def __init__(self,owner,balance):
+
+        self.owner = owner
+        self.balance = balance
+
+
+    def __str__(self):
+        return "Your name is: %s \nYour account balance is: $%s\n" % (self.owner, self.balance)
+
+
+    def deposit(self,deposit):
+        self.balance = self.balance + deposit
+        print("Deposit Accepted")
+
+
+    def withdraw(self,withdraw):
+        if (self.balance - withdraw) >= 0:
+            self.balance = self.balance - withdraw
+            print ("Withdrawl Accepted")
+            print ("Available Funds: $%s" % (self.balance))
+        else:
+            print ("Funds Unavailable!")
+
+
+#TEST THE CLASS ABOVE
+
+acct1 = Account('Jose',100)
+print (acct1)
+
+acct1.deposit(50)
+
+print (acct1)
+
+acct1.deposit(100)
+
+print (acct1)
+
+acct1.withdraw(50)
+
+print (acct1)
+
+acct1.withdraw(500)
+
+print (acct1)
+
